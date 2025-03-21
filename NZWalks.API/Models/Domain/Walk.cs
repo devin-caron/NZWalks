af70e5be8c794b2b89a00fed85944bade00ad1c9
devincar <devmcaron@gmail.com>
2025-03-21 13:14:21 -0700

    Created Domain Models & Ran EF Migrations

--- NZWalks.API/Models/Domain/Walk.cs
+++ NZWalks.API/Models/Domain/Walk.cs
@@ -2,5 +2,18 @@
 {
     public class Walk
     {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double LengthInKm { get; set; }
+        public string? WalkImageUrl { get; set; }
+
+        public Guid DiffucultyId { get; set; }
+
+        public Guid RegionId { get; set; }
+
+        //Navigation Properties
+        public Difficulity Difficulity { get; set; }
+        public Region Region { get; set; }
     }
 }

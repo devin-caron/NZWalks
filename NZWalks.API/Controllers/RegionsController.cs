b19a3efaf10c8bdc5ab758c9000d26ab5c060772
devincar <devmcaron@gmail.com>
2025-03-22 10:42:00 -0700

    Added GET region methods and added dto for best practices

--- NZWalks.API/Controllers/RegionsController.cs
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using NZWalks.API.Models.Domain;
+using NZWalks.API.Data;
+using NZWalks.API.Models.DTO;
 
 namespace NZWalks.API.Controllers
 {
@@ -7,28 +8,62 @@ namespace NZWalks.API.Controllers
     [ApiController]
     public class RegionsController : ControllerBase
     {
+        private readonly NZWalksDBContext dBContext;
+
+        public RegionsController(NZWalksDBContext dBContext)
+        {
+            this.dBContext = dBContext;
+        }
+
+        // GET ALL
         [HttpGet]
         public IActionResult GetAll()
         {
-            var regions = new List<Region>
+            // Get data from database - domain models
+            var regionsDomain = dBContext.Regions.ToList();
+
+            // Map domain models to DTOs
+            var regionsDto = new List<RegionDTO>();
+            foreach (var regionDomain in regionsDomain)
             {
-                new Region
+                regionsDto.Add(new RegionDTO()
                 {
-                    Id = Guid.NewGuid(),
-                    Name = "Auckland Region",
-                    Code = "AKL",
-                    RegionImageUrl = "https://images.pexels.com/photos/31256164/pexels-photo-31256164/free-photo-of-dramatic-coastal-landscape-in-auckland-new-zealand.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                },
-                new Region
-                {
-                    Id = Guid.NewGuid(),
-                    Name = "Wellington Region",
-                    Code = "WLG",
-                    RegionImageUrl = "https://images.pexels.com/photos/31256164/pexels-photo-31256164/free-photo-of-dramatic-coastal-landscape-in-auckland-new-zealand.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-                }
+                    Id = regionDomain.Id,
+                    Code = regionDomain.Code,
+                    Name = regionDomain.Name,
+                    RegionImageUrl = regionDomain.RegionImageUrl
+                });
+            }
+
+            // Return DTOs
+            return Ok(regionsDto);
+        }
+
+        // GET REGION BY ID
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public IActionResult GetById([FromRoute] Guid id)
+        {
+            //var region = dBContext.Regions.Find(id);
+
+            // Get region domain model from database
+            var regionDomain = dBContext.Regions.FirstOrDefault(x => x.Id == id);
+
+            if (regionDomain == null)
+            {
+                return NotFound();
+            }
+
+            // Map region domain model to region dto
+            var regionDto = new RegionDTO
+            {
+                Id = regionDomain.Id,
+                Code = regionDomain.Code,
+                Name = regionDomain.Name,
+                RegionImageUrl = regionDomain.RegionImageUrl
             };
 
-            return Ok(regions);
+            return Ok(regionDto);
         }
     }
 }
